4e62ba96238288329b8607c07fe6d7b497ea1aa1
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-01 09:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+function rotateClockHands() {
+  // Get current time
+  const currentTime = new Date();
+
+  // Calculate the degrees for each clock hand
+  const seconds = currentTime.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const minutes = currentTime.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const hours = currentTime.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+
+  // Rotate each clock hand using CSS transform property
+  const secondHand = document.querySelector('.second-hand');
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minuteHand = document.querySelector('.min-hand');
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hourHand = document.querySelector('.hour-hand');
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+// Update clock hands every second
+setInterval(rotateClockHands, 1000);
+
